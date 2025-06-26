d8c708a7051d69b78cbdbdc9a61981389b3dfd05
unknown <taiwokolapo533@gmail.com>
2025-06-26 23:28:12 +0100

    Udated

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -17,7 +17,7 @@ const Order = () => {
         <div key={order.id} className="shadow-lg flex flex-col gap-1.5 py-8  px-4 mb-3 rounded">
           <p><strong>{order.item}</strong> x{order.qty} for {order.customer}</p>
           <p>Status: {order.status}</p>
-          <div className="grid grid-cols-2 gap-2 space-x-2">
+          <div className="grid grid-cols-2 md:text-lg text-sm gap-2 space-x-2">
             <button className="bg-[#090040] text-white shadow- px-2 py-1 rounded" onClick={() => updateStatus(order.id, 'Preparing')}>Preparing</button>
             <button className="bg-blue-200 px-2 py-1 rounded" onClick={() => updateStatus(order.id, 'Out for Delivery')}>Out</button>
             <button className="bg-[#16610E] px-2 py-1 rounded" onClick={() => updateStatus(order.id, 'Delivered')}>Delivered</button>
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -19,17 +19,17 @@ const Product = () => {
   const deleteProduct = (id) => setProducts(products.filter(p => p.id !== id));
 
   return (
-    <div className="p-6">
-      <h2 className="text-xl font-semibold mb-4">Your Menu</h2>
+    <div className="p-6 bg-[#fefff5] h-screen text-whit">
+      <h2 className="text-xl font-semibold mb-4 ">Your Menu</h2>
       <div className="mb-4">
         <input type="text" placeholder="Product name" value={name} onChange={e => setName(e.target.value)} className="border p-2 rounded mr-2" />
         <input type="number" placeholder="₦ Price" value={price} onChange={e => setPrice(e.target.value)} className="border p-2 rounded mr-2" />
-        <button onClick={addProduct} className="bg-green-500 text-white px-4 py-2 rounded">Add</button>
+        <button onClick={addProduct} className="bg-green-500 text-white px-4 py-2 rounded cursor-pointer">Add</button>
       </div>
       {products.map(product => (
-        <div key={product.id} className="border p-4 mb-2 rounded flex justify-between items-center">
+        <div key={product.id} className="shadow p-4 mb-2 rounded flex justify-between items-center">
           <span>{product.name} - ₦{product.price}</span>
-          <button onClick={() => deleteProduct(product.id)} className="text-red-500">Delete</button>
+          <button onClick={() => deleteProduct(product.id)} className="text-white bg-[#ED3500] p-2 cursor-pointer rounded-lg">Delete</button>
         </div>
       ))}
     </div>
