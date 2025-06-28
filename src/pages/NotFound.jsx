367d50557f048553e993528bb79bb33f57e614d6
unknown <taiwokolapo533@gmail.com>
2025-06-28 23:00:19 +0100

    Updated

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div className='bg-white h-screen flex justify-center items-center'>
+    <div className='bg-black text-white h-screen flex justify-center items-center'>
         <h1>404 not Found</h1>
       
     </div>
